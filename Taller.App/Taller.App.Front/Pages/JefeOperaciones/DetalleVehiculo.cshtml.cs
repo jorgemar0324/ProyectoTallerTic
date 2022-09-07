373f3ed079adfefbe8066c9e2326c971cb5ec371
anmza <anmaza8402@gmail.com>
2022-09-06 23:03:27 -0500

    paginas sprint2

---
+++ Taller.App/Taller.App.Front/Pages/JefeOperaciones/DetalleVehiculo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Taller.App.Front.pages
+{
+    public class DetalleVehiculoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
