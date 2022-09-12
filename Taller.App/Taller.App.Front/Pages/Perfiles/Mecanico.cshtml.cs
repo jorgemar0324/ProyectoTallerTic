b1a380248ff28926dfca38dddb17acb656af8624
Jorge Mario Rivas <jorgemar0324@gmail.com>
2022-09-11 19:04:04 -0500

    Creacion paginas por perfil

---
+++ Taller.App/Taller.App.Front/Pages/Perfiles/Mecanico.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Taller.App.Front.Pages.Perfiles
+{
+    public class MecanicoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
